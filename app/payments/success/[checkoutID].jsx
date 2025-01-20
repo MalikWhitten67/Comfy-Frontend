4a369e6afa213da0e2346e161d4f8e1a58ea599d
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-01-20 11:03:04 -0600

    Further information

--- app/payments/success/[checkoutID].jsx
+++ app/payments/success/[checkoutID].jsx
@@ -10,8 +10,12 @@ export default function () {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </head>
             <body>
-                <div className="flex items-center justify-center min-h-screen bg-white">
+                <div className="flex flex-col items-center justify-center mx-auto min-h-screen bg-white">
                     <h1 className="text-2xl">Payment Successful</h1>
+                    <p>Thank you for your purchase. A confirmation email will be sent out soon!</p>
+                    <p>
+                        <a href="/member/orders">View Orders</a>
+                    </p>
                 </div>
 
             </body>
