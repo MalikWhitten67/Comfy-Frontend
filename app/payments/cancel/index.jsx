cd9b6e7f8b38796e7eb89db63b4979ab36df721e
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-01-20 10:52:07 -0600

    Delete app/payments/cancel/index.jsx

--- app/payments/cancel/index.jsx
+++
@@ -1,9 +0,0 @@
-export default function () {
-    return (
-        <div className="min-h-screen bg-white">
-            <h1>Payment Cancelled</h1>
-            <p>Your payment has been cancelled.</p>
-            <a href="/">Return to the app</a>
-        </div>
-    )
-}
