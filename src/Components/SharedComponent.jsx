8255389fb12bebbe2fbb45ded364c2c72a5e7d5e
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-01-21 17:12:21 -0600

    Update SharedComponent.jsx

--- src/Components/SharedComponent.jsx
+++ src/Components/SharedComponent.jsx
@@ -11,10 +11,12 @@ export default function({title, description, children}){
                 <link rel="preload" href="/public/models/rev1_acid/model2.webp" as="image" type="image/webp" crossorigin />
                 <link rel="preload" href="/public/models/rev1_acid/model3.webp" as="image" type="image/webp" crossorigin />
                 <link rel="preload" href="/public/models/rev1_acid/model4.webp" as="image" type="image/webp" crossorigin />
-                <link rel="preload" href="/public/shirts/comfy_v1.jpeg"  as="image" type="image/jpeg" crossorigin />
-                <link rel="preload" href="/public/shirts/comfy_v2_back.jpeg"  as="image" type="image/jpeg" crossorigin />
-                <link rel="preload" href="/public/shirts/comfy_v2.png"  as="image" type="image/png" crossorigin />
-                <link rel="preload" href="/public/shirts/shirt.webp"  as="image" type="image/webp" crossorigin />
+                <link rel="preload" href="/public/Products/shirts/comfy_v1.jpeg"  as="image" type="image/jpeg" crossorigin />
+                <link rel="preload" href="/public/Products/shirts/comfy_v2_back.jpeg"  as="image" type="image/jpeg" crossorigin />
+                <link rel="preload" href="/public/Products/shirts/comfy_v2.png"  as="image" type="image/png" crossorigin />
+                <link rel="preload" href="/public/Products/shirts/shirt.webp"  as="image" type="image/webp" crossorigin />
+
+                <meta name="description" content="Comfy - One of One" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
                 <link rel="shortcut icon" href="/public/Comfy_logo_V2.png" type="image/x-icon" />
