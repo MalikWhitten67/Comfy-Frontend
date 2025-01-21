3d7410c5f31a3f82911f79fbb4485a9d6fc46012
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-01-21 16:54:02 -0600

    Include description for metadata

--- src/Components/SharedComponent.jsx
+++ src/Components/SharedComponent.jsx
@@ -1,14 +1,15 @@
 import Footer from "./Footer";
 import Navbar from "./nav";
 
-export default function({title, children}){
+export default function({title, description, children}){
     return (
         <html>
             <head>
-                <title>Comfy - Favorites</title>
+                <title>{title || 'Comfy'}</title>
                 <meta name="description" content="Comfy - One of One" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+                {description && <meta name="description" content={description} />}
             </head>
             <body>
                 <Navbar />
