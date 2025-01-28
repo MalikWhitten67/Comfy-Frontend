eb4b8aaeb209bc7096e7d001443b78edb2f21226
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-01-28 10:29:10 -0600

    Update [id].jsx

--- products/[id].jsx
+++ products/[id].jsx
@@ -5,21 +5,26 @@ import Cart from "../../src/Sdk";
 import ItemAdded from "../../src/Components/ItemAdded";
 import SharedComponent from "../../src/Components/SharedComponent";
 import api from "../../src/api";
+import ReviewModal from "../../src/Components/ReviewProduct";
 export default function () {
     if (isServer) return <div></div>
     const cart = new Cart()
     let [product, setProduct] = useState(Products.items.find((product) => product.id === params.id) ? {
         id: params.id,
+        reviews: [],
         images: Products.items.find((product) => product.id === params.id).images,
         name: Products.items.find((product) => product.id === params.id).name,
         price: Products.items.find((product) => product.id === params.id).price,
         mainImage: Products.items.find((product) => product.id === params.id).mainImage,
         images: Products.items.find((product) => product.id === params.id).images,
+        description: Products.items.find((product)=> product.id === params.id).description,
         sizes: [],
         stock:[], 
-        quantity: 1
+        colors: [],
+        quantity: 1,
+        isForSale: false
     } : {
-        id: 0, images: [], name: '', price: 0, sizes: [], stock: []
+        id: 0, images: [], name: '', price: 0, sizes: [], stock: [], colors: [], quantity: 1
     })
     const [currentImageIndex, setCurrentImageIndex] = useState(0)
     let [favorite, setFavorite] = useState((cart.favorites.find((item) => item.id === params.id) ? true : false))
@@ -27,6 +32,7 @@ export default function () {
     const [error, setError] = useState(null)
     const [loader, setLoader] = useState(true)
     const [selectedSize, setSelectedSize] = useState(null)
+    const [selectedColor, setSelectedColor] = useState(null)
 
 
     const sizes = [
@@ -38,18 +44,30 @@ export default function () {
     ]
 
     function isOutofStock() {   
-        if(product.stock.length < 1) return true;
-        console.log( product.stock.every((stock)=> stock.quantity === 0))
+        if(product.stock.length < 1) return true; 
         return  product.stock.every((stock)=> stock.quantity === 0)
     }
-
-    console.log(loader)
-
-    useEffect(() => {
-        if (isServer) return 
-        api.collection("products").getOne(params.id).then((data) => {
-            console.log(data)
-            setProduct({ ...product, sizes: data.sizes, stock: data.stock })
+ 
+    useEffect(() => { 
+        api.collection("products").getOne(params.id, {
+            expand:'reviews',
+            headers:{
+                // cache for 20 minutes
+                'Cache-Control': 'max-age=1200'
+            }
+        }).then((data) => { 
+            api.collection("reviews").getFullList({
+                batch: Number.MAX_SAFE_INTEGER,
+                expand: 'author',
+                filter: `product="${params.id}"`,
+                headers:{
+                    // cache for 20 minutes
+                    'Cache-Control': 'max-age=1200'
+                }
+            }).then((d)=>{ 
+                setProduct({ ...product, sizes: data.sizes, stock: data.stock , colors: data.colors, reviews: d, isForSale: data.isForSale})
+            })
+             
             
         }).finally(()=>{
             setLoader(false)
@@ -71,8 +89,23 @@ export default function () {
             />
         </button>
     ))
+
+    function buyNow(){
+        if(product.id == "4ax9phi9m75dx2w"){
+            switch (true){
+                case selectedColor == "pink":
+                    window.location.href = "https://buy.stripe.com/4gw7uE1SW9pPdosfYZ"
+                    break;
+                case selectedColor == "black":
+                    window.location.href = "https://buy.stripe.com/dR64iscxA59zbgk6oo"
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
     return (
-        <SharedComponent title="Comfy - Product">
+        <SharedComponent title={`Comfy - ${product.name}`} description={product.description} image={product.images[0]} url={`https://comfy.vercel.app/products/${product.id}`} >
             <div className="mx-auto max-w-7xl px-4 py-8">
                 <div className="grid gap-8 md:grid-cols-2">
                     {/* Product Images */}
@@ -115,9 +148,13 @@ export default function () {
                         <div className="flex flex-col gap-6">
                             <div>
                                 <div className="mb-2 flex items-center gap-2">
-                                    <span className="inline-flex items-center rounded-full bg-gray-100 px-2 py-1 text-xs">
+                                     {
+                                        product.reviews.length > 0 && product.reviews.reduce((acc, review) => acc + review.overal_rating, 0) / product.reviews.length > 0 ? 
+                                        <span className="inline-flex items-center rounded-full bg-gray-100 px-2 py-1 text-xs">
                                         ★ Highly Rated
                                     </span>
+ : ''
+                                     }
                                 </div>
                                 <h1 className="text-3xl font-bold">{product.name}</h1>
                                 <p className="text-xl text-gray-500">${product.price}</p>
@@ -132,12 +169,14 @@ export default function () {
                                             <button
                                                 key={size}
                                                 onClick={() => {
-                                                    if (product.stock.find((stock) => stock.size.toLowerCase() === size.toLowerCase())?.quantity === 0) return
-                                                    setSelectedSize(size)
-                                                }}
-                                                className={`rounded-full   px-6 py-4 ${error && error === 'size' ? 'border border-red-500' : selectedSize === size ? 'border-black border' : 'hover:border-black border border-gray-300'
+                                                    if(product.isForSale) 
+                                                        setSelectedSize(size);
+                                                }} 
+                                                className={`rounded-full   px-6 py-4 ${error && error === 'size' ? 'border border-red-500' : selectedSize === size ? 'border-black border' : product.isForSale ?  'hover:border-black border border-gray-300' : ''
                                                     }
-                                                    ${product.stock.find((stock) => stock.size.toLowerCase() === size.toLowerCase())?.quantity === 0 && 'bg-gray-200 text-gray-400 cursor-not-allowed'
+                                                     ${
+                                                        !product.isForSale && 'bg-gray-200'
+                                                     }
                                                     }
                                                     `}
                                             >
@@ -149,31 +188,37 @@ export default function () {
                                 </div>
                             </div>
 
+
+                            {/* choose color */}
+                            <div>
+                                <h2 className="mb-4 text-lg font-semibold">Choose Color</h2>
+                                <div className="flex gap-5">
+                                { 
+                                    loader ? <div>Loading...</div> : product.colors.map((stock) => (
+                                         
+                                        <span className="w-12 h-12 cursor-pointer rounded-full inline-block" style={{ backgroundColor: stock.hex ,  border: selectedColor === stock.color  && error && error === 'color' ? '2px solid red' : selectedColor === stock.color ? '4px solid white' : '2px solid transparent'
+                                         }}
+                                        onClick={() => { 
+                                            setSelectedColor(stock.color)
+                                            console.log(stock.color)
+                                        }}
+                                        ></span>
+                                    ))
+                                }
+                                </div>
+                            </div>
                             {/* Add to Bag & Favorite */}
                             <div className="flex flex-col gap-4">
-                                <button className="w-full rounded-full bg-black px-6 py-4 text-white hover:bg-gray-800"
-                                    onClick={() => {
-                                        if (product.canBuyMultiple == false && cart.items.find((item) => item.id === product.id)) {
-                                            alert('You can only buy one of this product')
-                                            return
-                                        }
-                                        if (isOutofStock()) {
-                                            return
-                                        }
-                                        if (!selectedSize) {
-                                            setError('size')
-                                            setTimeout(() => setError(null), 2000)
-                                            return
-                                        }
-                                        cart.addItem({ ...product, size: selectedSize })
-                                        console.log(cart.items)
-                                        document.getElementById('item-added').showModal()
-                                    }}
-                                >
-                                    {
-                                        loader ? 'Loading....' : isOutofStock() ? 'Out of Stock' : 'Add to Bag'
-                                    }
-                                </button>
+                                <Switch>
+                                    <Match when={product.id === "4ax9phi9m75dx2w"}>
+                                        <button className="w-full rounded-full bg-black px-6 py-4 text-white hover:bg-gray-800"
+                                        onClick={buyNow}
+                                        >
+                                            Pre-Order
+                                        </button>
+                                    </Match>
+                                </Switch>
+                                
                                 <button
                                     onClick={() => {
                                         cart.toggleFavoriteItem(product) 
@@ -192,13 +237,64 @@ export default function () {
                              </p>
                             </div>
 
+                            <div className="border-t pt-6">
+                            <h3 className="mb-2 font-semibold">Reviews</h3>
+                            <Switch>
+                                <Match when={product.reviews.length === 0}>
+                                    <p className="text-sm text-gray-600">No reviews yet.</p>
+                                    <button className="text-sm text-gray-600 underline"  onClick={() => {
+                                            if(!api.authStore.isValid) return alert('Please login to write a review')
+                                            document.getElementById('reviewProduct').showModal()
+                                        }}>
+                                        Be the first to review
+                                    </button>
+                                </Match>
+                                <Match when={product.reviews.length > 0}>
+                                    <div className="flex flex-col gap-2 mb-2">
+                                        <div className="flex items-center gap-2">
+                                        <button className="text-sm text-gray-600 underline" onClick={() => {
+                                            if(!api.authStore.isValid) return alert('Please login to write a review')
+                                            document.getElementById('reviewProduct').showModal()
+                                        }}>
+                                            Write a review
+                                        </button>
+                                        <span className="inline-flex items-center rounded-full bg-gray-100 px-2 py-1 text-xs">
+                                            ★★★★★
+                                        </span>
+                                        <span className="text-sm text-gray-600">
+                                            {product.reviews.length} reviews
+                                        </span>
+                                        </div>
+                                        {
+                                        loader ? 'Loading...' : product.reviews.map((review) => (
+                                            <div key={review.id} className="border-b py-4">
+                                                <h4 className="font-semibold">{""}- <span
+                                                className="text-yellow-500"
+                                                > {new Array(review.overal_rating).fill('★').join('')}</span></h4>
+                                                <p className="text-sm text-gray-600">{review.description}</p> 
+                                                <p className="text-sm text-gray-600">Size: {review.size_you_normally_wear}</p>
+                                            </div>
+                                        ))
+                                     }
+                                    </div>
+
+                                     
+                                </Match>
+                            </Switch>
+                        </div>
+
                             {/* Shipping Info */}
                             <div className="border-t pt-6">
                                 <h3 className="mb-2 font-semibold">Shipping</h3>
+                                <p>
+                                    Please allow 1-2 weeks for shipping.
+                                </p>
                                 <p className="text-sm text-gray-600">
                                     You&apos;ll see our shipping options at checkout.
                                 </p>
                             </div>
+
+
 
                             {/* Store Pickup */}
                             <div className="border-t pt-6">
@@ -211,6 +307,7 @@ export default function () {
                     </div>
                 </div>
                 <ItemAdded />
+                <ReviewModal product={product}  setProduct={setProduct} />
         </SharedComponent>
 
     )
