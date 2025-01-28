13b561a1b4705f4ea51186aefc82a3601f5a07e1
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-01-28 10:41:39 -0600

    Update [checkoutID].jsx

--- app/payments/cancel/[checkoutID].jsx
+++ app/payments/cancel/[checkoutID].jsx
@@ -1,7 +1,24 @@
-export default function(){
-   let { checkoutID} = params;
+import api from "../../../src/api"
+export default function () {
+    let { checkoutID } = params
+ 
+    return (
+        <html>
+            <head>
+                <title>Comfy - Payment Success</title>
+                <meta name="description" content="Comfy - Payment Success" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </head>
+            <body>
+                <div className="flex flex-col items-center justify-center mx-auto min-h-screen bg-white">
+                    <h1 className="text-2xl">Order Cancelled Successful</h1>
+                    <p>AWW. Sad to see you go - your order has been cancelled!</p>
+                    <p>
+                        <a href="/">Go Back Home</a>
+                    </p>
+                </div>
 
-  return (
-    <div></div>
-  )
+            </body>
+        </html>
+    )
 }
