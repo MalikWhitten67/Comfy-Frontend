84eb99545ac63eda3c32991e63b868a413ff9320
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-10-19 18:48:20 -0700

    Delete vader.config.ts

--- vader.config.ts
+++
@@ -1,7 +0,0 @@
-import defineConfig from 'vaderjs/config'
-import tailwindcss from 'vaderjs/plugins/tailwind'
-export default  defineConfig({
-    port: 3001,
-    host_provider: 'vercel',
-    plugins:[tailwindcss]
-})
